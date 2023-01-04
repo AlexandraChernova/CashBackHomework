bef91abb4b8cd80b5c30b37a0a3769206c28af45
Alexandra Chernova <chernova.aleksandra28@gmail.com>
2023-01-05 01:16:52 +0300

    change tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,8 +8,30 @@ class CashbackHackServiceTest {
 
     @Test
     public void calculateBonus() {
-        int amount = 2000;
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void calculateBonusIfZero() {
+        int amount = 0;
         int expected = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void calculateAnotherBonus() {
+        int amount = 999;
+        int expected = 1;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
 
