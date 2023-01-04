f9dea431fa049ae992a37e8e51e452a27ecc4058
Alexandra Chernova <chernova.aleksandra28@gmail.com>
2023-01-05 01:13:33 +0300

    change test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,7 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
     @Test
     public void calculateBonus(){
-        int amount = 2000;
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void calculateBonusIfZero(){
+        int amount = 0;
         int expected = 1000;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
